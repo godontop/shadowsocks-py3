d83a19a6354bbb3bcefda2f494c15f028cfd3eb9
godontop <tosony@126.com>
2017-12-30 23:29:35 +0800

    server source code file

--- shadowsocks/server.py
+++ shadowsocks/server.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os.path
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
 from shadowsocks import utils
 
 
 def main():
     utils.check_python()
+    config = utils.get_config(False)
 
 
 if __name__ == '__main__':
